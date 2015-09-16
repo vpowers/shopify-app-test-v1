6ad575e7ab32cc2cf72f519324773c40f47114b2
vpowers <vic.powers@gmail.com>
2015-09-16 10:03:25 -0500

    adding apikey

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.api_key = "<api_key>"
+  config.api_key = "bdfe213d705c6b11973703b4c92a9271"
   config.secret = "d7318927fb493eb5924723f9f960a6d2"
   config.redirect_uri = "<redirect_uri>"
   config.scope = "read_orders, read_products"
