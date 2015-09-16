6cec1c3dafa4dee654d0b7b5d46e36fcb7aa4db3
vpowers <vic.powers@gmail.com>
2015-09-15 20:00:44 -0500

    creating repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopifyAppTest::Application.config.secret_key_base = '2c76c25489462a017d494942bb2057136fb877ab7cb87de89ad4b101603fc20db16b3d07d576b7655b4dc047aeab3788cc635ef0b3e430be730105fd7156e5fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShopifyAppTest::Application.config.session_store :cookie_store, key: '_shopify-app-test_session'
