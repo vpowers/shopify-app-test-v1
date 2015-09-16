bf7ac222cd517c2398eb7d23f368d4da627f00b1
vpowers <vic.powers@gmail.com>
2015-09-16 10:26:02 -0500

    changing redirect_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "bdfe213d705c6b11973703b4c92a9271"
   config.secret = "d7318927fb493eb5924723f9f960a6d2"
-  config.redirect_uri = "<redirect_uri>"
+  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
